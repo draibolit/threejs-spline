7ac96db77978570d5d82c1afb5eed0aa6badcecb
tuan <tuannguyenrhm@gmail.com>
2020-12-29 07:18:47 +0900

    modulize spline

--- src/index.js
+++ src/index.js
@@ -1,203 +1,106 @@
 "use strict";
 import * as THREE from "three";
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
+import Stats from "three/examples/jsm/libs/stats.module.js";
 
-import { addSplineObject } from './spline';
+import { Spline } from "./spline";
 
 let stats;
 let camera, scene, renderer;
-const splineHelperObjects = [];
-const positions = [];
-let splinePointsLength = 4;
-const point = new THREE.Vector3();
 const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
 const onUpPosition = new THREE.Vector2();
 const onDownPosition = new THREE.Vector2();
-let transformControl;
-const ARC_SEGMENTS = 200;
-const params = {
-  uniform: true,
-  tension: 0.5,
-  addPoint: addPoint,
-  removePoint: removePoint,
-  exportSpline: exportSpline
-};
-
+let pointer = new THREE.Vector2();
 let spline;
 
 init();
 animate();
 
 function init() {
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMap.enabled = true;
 
   document.body.appendChild(renderer.domElement);
   stats = new Stats();
-  document.body.appendChild( stats.dom );
+  document.body.appendChild(stats.dom);
   scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0xf0f0f0 );
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.set( 0, 250, 1000 );
+  scene.background = new THREE.Color(0xf0f0f0);
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
+  camera.position.set(0, 250, 1000);
 
-  scene.add( camera );
-  scene.add( new THREE.AmbientLight( 0xf0f0f0 ) );
+  scene.add(camera);
+  scene.add(new THREE.AmbientLight(0xf0f0f0));
 
-  const light = new THREE.SpotLight( 0xffffff, 1.5 );
-  light.position.set( 0, 1500, 200 );
+  const light = new THREE.SpotLight(0xffffff, 1.5);
+  light.position.set(0, 1500, 200);
   light.angle = Math.PI * 0.2;
   light.castShadow = true;
   light.shadow.camera.near = 200;
   light.shadow.camera.far = 2000;
-  light.shadow.bias = - 0.000222;
+  light.shadow.bias = -0.000222;
   light.shadow.mapSize.width = 1024;
   light.shadow.mapSize.height = 1024;
-  scene.add( light );
+  scene.add(light);
 
-  const planeGeometry = new THREE.PlaneBufferGeometry( 2000, 2000 );
-  planeGeometry.rotateX( - Math.PI / 2 );
-  const planeMaterial = new THREE.ShadowMaterial( { opacity: 0.2 } );
-  const plane = new THREE.Mesh( planeGeometry, planeMaterial );
-  plane.position.y = - 200;
+  const planeGeometry = new THREE.PlaneBufferGeometry(2000, 2000);
+  planeGeometry.rotateX(-Math.PI / 2);
+  const planeMaterial = new THREE.ShadowMaterial({ opacity: 0.2 });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.position.y = -200;
   plane.receiveShadow = true;
-  scene.add( plane );
+  scene.add(plane);
 
-  const helper = new THREE.GridHelper( 2000, 100 );
-  helper.position.y = - 199;
+  const helper = new THREE.GridHelper(2000, 100);
+  helper.position.y = -199;
   helper.material.opacity = 0.25;
   helper.material.transparent = true;
-  scene.add( helper );
+  scene.add(helper);
 
-  const gui = new GUI();
-  gui.add( params, 'uniform' );
-  gui.add( params, 'tension', 0, 1 ).step( 0.01 ).onChange( function ( value ) {
-    spline.tension = value;
-    updateSplineOutline();
-  } );
-  gui.add( params, 'addPoint' );
-  gui.add( params, 'removePoint' );
-  gui.add( params, 'exportSpline' );
-  gui.open();
-
-  // Controls
-  const controls = new OrbitControls( camera, renderer.domElement );
-  controls.damping = 0.2;
-  controls.addEventListener( 'change', render );
-  transformControl = new TransformControls( camera, renderer.domElement );
-  transformControl.addEventListener( 'change', render );
-  transformControl.addEventListener( 'dragging-changed', function ( event ) {
-    controls.enabled = ! event.value;
-  } );
-  scene.add( transformControl );
-  transformControl.addEventListener( 'objectChange', function () {
-    updateSplineOutline();
-  } );
-
-  document.addEventListener( 'pointerdown', onPointerDown, false );
-  document.addEventListener( 'pointerup', onPointerUp, false );
-  document.addEventListener( 'pointermove', onPointerMove, false );
+  document.addEventListener("pointerdown", onPointerDown, false);
+  document.addEventListener("pointerup", onPointerUp, false);
+  document.addEventListener("pointermove", onPointerMove, false);
 
   /*******
    * Curves
    *********/
-  for ( let i = 0; i < splinePointsLength; i ++ ) {
-    let obj = addSplineObject( positions[ i ] );
-    splineHelperObjects.push(obj);
-    scene.add(obj);
-  }
 
-  // positions.length = 0;
-  for ( let i = 0; i < splinePointsLength; i ++ ) {
-    positions.push( splineHelperObjects[ i ].position );
-  }
-
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute( 'position', new THREE.BufferAttribute( new Float32Array( ARC_SEGMENTS * 3 ), 3 ) );
-
-  spline = new THREE.CatmullRomCurve3( positions );
-  spline.mesh = new THREE.Line( geometry.clone(), new THREE.LineBasicMaterial( {
-    color: 0xff0000,
-    opacity: 0.35
-  } ) );
-
-  scene.add(spline.mesh);
-
-  load( [ new THREE.Vector3( 289.76843686945404, 452.51481137238443, 56.10018915737797 ),
-    new THREE.Vector3( - 53.56300074753207, 171.49711742836848, - 14.495472686253045 ),
-    new THREE.Vector3( - 91.40118730204415, 176.4306956436485, - 6.958271935582161 ),
-    new THREE.Vector3( - 383.785318791128, 491.1365363371675, 47.869296953772746 ) ] );
-}
-
-function addPoint() {
-  splinePointsLength ++;
-  let obj = addSplineObject();
-  splineHelperObjects.push(obj);
-  scene.add(obj);
-  positions.push( obj.position );
-  updateSplineOutline();
-}
-
-function removePoint() {
-  if ( splinePointsLength <= 4 ) {
-    return;
-  }
-  const point = splineHelperObjects.pop();
-  splinePointsLength --;
-  positions.pop();
-  if ( transformControl.object === point ) transformControl.detach();
-  scene.remove( point );
-  updateSplineOutline();
-}
-function updateSplineOutline() {
-  const position = spline.mesh.geometry.attributes.position;
-  for ( let i = 0; i < ARC_SEGMENTS; i ++ ) {
-    const t = i / ( ARC_SEGMENTS - 1 );
-    spline.getPoint( t, point );
-    position.setXYZ( i, point.x, point.y, point.z );
-  }
-  position.needsUpdate = true;
-}
-function exportSpline() {
-  const strplace = [];
-  for ( let i = 0; i < splinePointsLength; i ++ ) {
-    const p = splineHelperObjects[ i ].position;
-    strplace.push( `new THREE.Vector3(${p.x}, ${p.y}, ${p.z})` );
-  }
-  console.log( strplace.join( ',\n' ) );
-  const code = '[' + ( strplace.join( ',\n\t' ) ) + ']';
-  prompt( 'copy and paste code', code );
-}
-
-function load( new_positions ) {
-  while ( new_positions.length > positions.length ) {
-    addPoint();
-  }
-  while ( new_positions.length < positions.length ) {
-    removePoint();
-  }
-  for ( let i = 0; i < positions.length; i ++ ) {
-    positions[ i ].copy( new_positions[ i ] );
-  }
-  updateSplineOutline();
+  spline = new Spline(scene, camera, renderer, [
+    new THREE.Vector3(
+      289.76843686945404,
+      452.51481137238443,
+      56.10018915737797
+    ),
+    new THREE.Vector3(
+      -53.56300074753207,
+      171.49711742836848,
+      -14.495472686253045
+    ),
+    new THREE.Vector3(
+      -91.40118730204415,
+      176.4306956436485,
+      -6.958271935582161
+    ),
+    new THREE.Vector3(-383.785318791128, 491.1365363371675, 47.869296953772746),
+  ]);
 }
 
 function animate() {
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
   render();
   stats.update();
 }
 
 function render() {
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
-function onPointerDown( event ) {
+function onPointerDown(event) {
   onDownPosition.x = event.clientX;
   onDownPosition.y = event.clientY;
 }
@@ -205,18 +108,19 @@ function onPointerDown( event ) {
 function onPointerUp(event) {
   onUpPosition.x = event.clientX;
   onUpPosition.y = event.clientY;
-  if ( onDownPosition.distanceTo( onUpPosition ) === 0 ) transformControl.detach();
+  if (onDownPosition.distanceTo(onUpPosition) === 0)
+    spline.transformControl.detach();
 }
 
-function onPointerMove( event ) {
-  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-  raycaster.setFromCamera( pointer, camera );
-  const intersects = raycaster.intersectObjects( splineHelperObjects );
-  if ( intersects.length > 0 ) {
-    const object = intersects[ 0 ].object;
-    if ( object !== transformControl.object ) {
-      transformControl.attach( object );
+function onPointerMove(event) {
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(pointer, camera);
+  const intersects = raycaster.intersectObjects(spline.splineHelperObjects);
+  if (intersects.length > 0) {
+    const object = intersects[0].object;
+    if (object !== spline.transformControl.object) {
+      spline.transformControl.attach(object);
     }
   }
 }
--- src/spline.js
+++ src/spline.js
@@ -1,23 +1,151 @@
-
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { TransformControls } from "three/examples/jsm/controls/TransformControls.js";
+import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
 
-const geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
+class Spline {
+  constructor(scene, camera, renderer, new_positions) {
+    this.boxGeo = new THREE.BoxBufferGeometry(20, 20, 20); //const
+    this.splineHelperObjects = [];
+    this.splinePointsLength = 4; //const
+    this.positions = [];
+    this.point = new THREE.Vector3();
+    this.ARC_SEGMENTS = 200; //const
 
-function addSplineObject( position ) {
-  const material = new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } );
-  const object = new THREE.Mesh( geometry, material );
-  if ( position ) {
-    object.position.copy( position );
-  } else {
-    object.position.x = Math.random() * 1000 - 500;
-    object.position.y = Math.random() * 600;
-    object.position.z = Math.random() * 800 - 400;
+    this.scene = scene;
+
+    for (let i = 0; i < this.splinePointsLength; i++) {
+      let obj = this.addSplineObject(this.positions[i]);
+      this.splineHelperObjects.push(obj);
+      scene.add(obj);
+    }
+
+    for (let i = 0; i < this.splinePointsLength; i++) {
+      this.positions.push(this.splineHelperObjects[i].position);
+    }
+
+    let curveGeo = new THREE.BufferGeometry();
+    curveGeo.setAttribute(
+      "position",
+      new THREE.BufferAttribute(new Float32Array(this.ARC_SEGMENTS * 3), 3)
+    );
+
+    this.initControls(camera, renderer);
+
+    let scope = this;
+    // Gui
+    const params = {
+      uniform: true,
+      tension: 0.5,
+      addPoint: scope.addPoint.bind(scope),
+      removePoint: scope.removePoint.bind(scope),
+    };
+
+    const gui = new GUI();
+    gui.add(params, "uniform");
+    gui
+      .add(params, "tension", 0, 1)
+      .step(0.01)
+      .onChange(function (value) {
+        spline.tension = value;
+        updateSplineOutline();
+      });
+    gui.add(params, "addPoint");
+    gui.add(params, "removePoint");
+    gui.open();
+
+    this.spline = new THREE.CatmullRomCurve3(this.positions);
+    this.spline.mesh = new THREE.Line(
+      curveGeo.clone(),
+      new THREE.LineBasicMaterial({
+        color: 0xff0000,
+        opacity: 0.35,
+      })
+    );
+    this.scene.add(this.spline.mesh);
+
+    this.load(new_positions);
   }
-  object.castShadow = true;
-  object.receiveShadow = true;
-  // scene.add( object );
-  // splineHelperObjects.push( object );
-  return object;
+
+  load(new_positions) {
+    while (new_positions.length > this.positions.length) {
+      this.addPoint();
+    }
+    while (new_positions.length < this.positions.length) {
+      this.removePoint();
+    }
+    for (let i = 0; i < this.positions.length; i++) {
+      this.positions[i].copy(new_positions[i]);
+    }
+    this.updateSplineOutline();
+  }
+
+  initControls(camera, renderer) {
+    this.controls = new OrbitControls(camera, renderer.domElement);
+    this.controls.damping = 0.2;
+    // this.controls.addEventListener( 'change', render );
+    this.transformControl = new TransformControls(camera, renderer.domElement);
+    // this.transformControl.addEventListener( 'change', render );
+    let scope = this;
+    this.transformControl.addEventListener(
+      "dragging-changed",
+      function (event) {
+        scope.controls.enabled = !event.value;
+      }
+    );
+    this.scene.add(this.transformControl);
+    this.transformControl.addEventListener("objectChange", function () {
+      scope.updateSplineOutline();
+    });
+  }
+
+  addSplineObject(position) {
+    const material = new THREE.MeshLambertMaterial({
+      color: Math.random() * 0xffffff,
+    });
+    const object = new THREE.Mesh(this.boxGeo, material);
+    if (position) {
+      object.position.copy(position);
+    } else {
+      object.position.x = Math.random() * 1000 - 500;
+      object.position.y = Math.random() * 600;
+      object.position.z = Math.random() * 800 - 400;
+    }
+    object.castShadow = true;
+    object.receiveShadow = true;
+    return object;
+  }
+
+  addPoint() {
+    this.splinePointsLength++;
+    let obj = this.addSplineObject();
+    this.splineHelperObjects.push(obj);
+    this.scene.add(obj);
+    this.positions.push(obj.position);
+    this.updateSplineOutline();
+  }
+
+  removePoint() {
+    if (this.splinePointsLength <= 4) {
+      return;
+    }
+    const point = this.splineHelperObjects.pop();
+    this.splinePointsLength--;
+    this.positions.pop();
+    if (this.transformControl.object === point) this.transformControl.detach();
+    this.scene.remove(point);
+    this.updateSplineOutline();
+  }
+
+  updateSplineOutline() {
+    const position = this.spline.mesh.geometry.attributes.position;
+    for (let i = 0; i < this.ARC_SEGMENTS; i++) {
+      const t = i / (this.ARC_SEGMENTS - 1);
+      this.spline.getPoint(t, this.point);
+      position.setXYZ(i, this.point.x, this.point.y, this.point.z);
+    }
+    position.needsUpdate = true;
+  }
 }
 
-export { addSplineObject};
+export { Spline };
